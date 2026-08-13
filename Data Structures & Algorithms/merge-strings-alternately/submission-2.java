ddbb7714ff44303737c4c637023d42cf317abbab
Mai Văn Lượng <fanadn2019@gmail.com>
2026-08-13 15:15:07 +0700

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String res = "";
+        int i = 0, j = 0;
+        while( i < word1.length() || j < word2.length()){
+            if(i < word1.length()){
+                res += word1.charAt(i);
+                i++;
+            }
+            if(j < word2.length()){
+                res += word2.charAt(j);
+                j++;
+            }
+        }
+        return res;
+    }
+}
