c8e540ea55aa7aab27825b1e7ef0c6e2ab81f870
Mai Văn Lượng <fanadn2019@gmail.com>
2026-08-14 15:47:39 +0700

    Add: 4sum - submission-11

---
+++ Data Structures & Algorithms/4sum/submission-11.java
@@ -0,0 +1,63 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // bỏ duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // bỏ duplicate j
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                long x = (long) target - nums[i] - nums[j];
+
+                while (left < right) {
+                    long y = (long) nums[left] + nums[right];
+
+                    if (y == x) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+
+                        res.add(list);
+
+                        left++;
+                        right--;
+
+                        // bỏ duplicate left
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        // bỏ duplicate right
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+
+                    } else if (y < x) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
